a4aad04d29fe69c3dd70024bcb684b08d0714361
chrisjg77 <chrisg@terraeclipse.com>
2014-06-23 16:10:13 -0700

    Adds isotope and styles for grid blocks

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+(function() {
+  $(function() {
+
+    $('.stream-grid').isotope({
+      itemSelector: '.stream-grid-block',
+      layoutMode: 'masonry',
+      columnWidth: '25%'
+    });
+
+  });
+}).call(this);;
